b066fc519ace7abf6de859f5a1dccc36dc40fc17
Faiza Khan <161170273+faizak2521@users.noreply.github.com>
2025-10-03 17:18:17 -0500

    Added API to replace random numbers logic with fallback options in place

--- script.js
+++ script.js
@@ -11,10 +11,35 @@ window.onload = () => {
 const dice = document.querySelector('.dice');
 const rollBtn = document.querySelector('.roll-btn');
 
+/* Replace with API
 const randomDice = () => {
     const random = Math.floor(Math.random() * 6) + 1; // 1..6 inclusive
     rollDice(random);
 }
+*/
+
+/* Fetch API with best practices such as fallback option if API fails */
+async function randomDice() {
+    try {
+        // Call API
+        const res = await fetch('https://dice-roller-api-fak-cybpc2h6djf5c5ex.canadacentral-01.azurewebsites.net/api/random?min=1&max=6');
+
+        // Await response
+        const data = await res.json();
+
+        // Extract the rand value
+        rollDice(data.value);
+    }
+    // Error handeling
+    catch (err) {
+        console.error("API call failing, fallback to local randomizer");
+        const random = Math.floor(Math.random() * 6) + 1; // fallback
+        rollDice(random);
+
+    }
+}
+
+
 
 const rollDice = random => {
     // reset animation so it can retrigger
